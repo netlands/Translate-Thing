cdc69e961d74e4b2c6847d0c150c180bb7e16833
barry <barry@netlands.com>
2021-08-15 00:03:46 +0900

    Cleanup formatting

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = require('better-sqlite3')('tm.db');
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
-app.use( express.static( "public" ) );
+app.use(express.static("public"));
 
 
 // prepare server
@@ -17,15 +17,17 @@ app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redi
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 // index page
-app.get('/', function(req, res) {
-    res.render('page/index', {result_from_database: res.body} );
+app.get('/', function (req, res) {
+  res.render('page/index', {
+    result_from_database: res.body
+  });
 });
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
 });
 
-app.get('/api/translate', function(req, res) {
+app.get('/api/translate', function (req, res) {
   //console.log(req.query);
   res.json({
     message: translateString(req.query.original)
@@ -39,11 +41,11 @@ function translateString(original) {
   var result, translation;
   translations = "";
 
-  while((result = regex.exec(content)) !== null) {
+  while ((result = regex.exec(content)) !== null) {
     let name = result[1].trim();
     let value = result[2].trim();
-    console.log(name," : ",value);
-    
+    console.log(name, " : ", value);
+
     translation = "";
     const row = db.prepare('SELECT * FROM glossary WHERE en = ? COLLATE NOCASE').get(name);
     if (row !== undefined) {
@@ -58,7 +60,7 @@ function translateString(original) {
 
     const row2 = db.prepare('SELECT * FROM glossary WHERE en = ? COLLATE NOCASE').get(value);
     if (row2 !== undefined) {
-        translation = translation + row2.ja + "\n";
+      translation = translation + row2.ja + "\n";
     } else {
       translation = translation + value + "\n"
     }
--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,24 @@
 console.log("connected");
 
-    $(document).ready(function () {
-        $("#HButton").on("click", function () {
-          console.log("Hello there!");
-          var original = $("#original").val();
-          $.ajax({
-             url: "/api/translate",
-             data: { original: original },
-             success: function() {
-                 console.log('Successfully connected to the server');
-             }, 
-             error: function() {
-                 console.log('Something went wrong');
-             }
-          })
-          .done(function( data ) {
-             console.log( "returned data:", data );
-             $('#translation').html(data.message);
-          });
-        });
+$(document).ready(function () {
+    $("#HButton").on("click", function () {
+        console.log("Hello there!");
+        var original = $("#original").val();
+        $.ajax({
+                url: "/api/translate",
+                data: {
+                    original: original
+                },
+                success: function () {
+                    console.log('Successfully connected to the server');
+                },
+                error: function () {
+                    console.log('Something went wrong');
+                }
+            })
+            .done(function (data) {
+                console.log("returned data:", data);
+                $('#translation').html(data.message);
+            });
     });
+});

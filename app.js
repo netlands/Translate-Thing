88bf275b050efe90100343fc27b5dc4247432388
barry <barry@netlands.com>
2021-08-16 17:48:43 +0900

    Fixes to Glossary Function

--- app.js
+++ app.js
@@ -14,91 +14,143 @@ app.use('/', express.static(__dirname + '/www')); // redirect root
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/js', express.static(__dirname + '/node_modules/gridjs/dist')); // redirect gridjs
+app.use('/css', express.static(__dirname + '/node_modules/gridjs/dist/theme')); // redirect gridjs
 
 // index page
 app.get('/', function (req, res) {
-  res.render('page/index', {
-    result_from_database: res.body
-  });
+	res.render('page/index', {
+		result_from_database: res.body
+	});
 });
 
 // start server
 app.listen(3000, function () {
-  console.log('App active on http://127.0.0.1:3000 and http://' + getLocalIp() + ':3000');
-  open('http://127.0.0.1:3000');
+	console.log('App active on http://127.0.0.1:3000 and http://' + getLocalIp() + ':3000');
+	open('http://127.0.0.1:3000');
 });
 
 
 app.get('/api/translate', function (req, res) {
-  //console.log(req.query);
-  res.json({
-    message: translateString(req.query.original)
-  });
+	//console.log(req.query);
+	res.json({
+		translation: translateString(req.query.original)
+	});
 });
 
 
 app.get('/api/addterm', function (req, res) {
-  // console.log(req.query);
-  addTerm(req.query.en, req.query.ja, req.query.furigana, req.query.romaji, req.query.ja2, req.query.en2, req.query.context, req.query.type, req.query.note);
-  res.json({
-    message: "term added"
-  });
+	// console.log(req.query);
+	addTerm(req.query.en, req.query.ja, req.query.furigana, req.query.romaji, req.query.ja2, req.query.en2, req.query.context, req.query.type, req.query.note);
+	res.json({
+		message: "term added"
+	});
+});
+
+app.get('/api/getterm', function (req, res) {
+	// http://127.0.0.1:3000/api/getterm?term=silk
+	const stmt = db.prepare('SELECT * FROM glossary WHERE en = ? OR en2 = ? OR romaji = ?');
+	const row = stmt.get(req.query.term, req.query.term, req.query.term);
+
+	if (row === undefined) {
+		const stmt2 = db.prepare('SELECT * FROM glossary WHERE ja = ? OR ja2 = ? OR furigana = ?');
+		const row2 = stmt2.get(req.query.term, req.query.term, req.query.term);
+		if (row2 === undefined) {
+			res.json({
+				translation: "NO MATCH"
+			});
+		} else {
+			res.json({
+				translation: row2.en
+			});
+		}
+
+	} else {
+		res.json({
+			translation: row.ja
+		});
+	}
+});
+
+app.get('/api/getrows', function (req, res) {
+	// http://127.0.0.1:3000/api/getterm?term=silk
+	const stmt = db.prepare('SELECT * FROM glossary WHERE en = ? OR en2 = ? OR romaji = ?');
+	const row = stmt.all(req.query.term, req.query.term, req.query.term);
+
+	if (row === undefined) {
+		const stmt2 = db.prepare('SELECT * FROM glossary WHERE ja = ? OR ja2 = ? OR furigana = ?');
+		const row2 = stmt2.all(req.query.term, req.query.term, req.query.term);
+		if (row2 === undefined) {
+			res.json({
+				rows: "NO MATCH"
+			});
+		} else {
+			res.json({
+				rows: row2
+			});
+		}
+
+	} else {
+		res.json({
+			rows: row
+		});
+	}
 });
 
 
 function addTerm(en, ja, furigana, romaji, ja2, en2, context, type, note) {
-  const stmt = db.prepare('INSERT INTO glossary (en,ja,furigana,romaji,ja2,en2,context,type,note) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
-  const info = stmt.run(en, ja, furigana, romaji, ja2, en2, context, type, note);
-  // console.log(info.changes); // => 1
+	const stmt = db.prepare('INSERT INTO glossary (en,ja,furigana,romaji,ja2,en2,context,type,note) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
+	const info = stmt.run(en, ja, furigana, romaji, ja2, en2, context, type, note);
+	// console.log(info.changes); // => 1
 }
 
 
 function translateString(original) {
-  content = original;
-  // console.log(content);
-  var regex = new RegExp(/^([A-Za-z\-/ ]+): (.+)$/gm);
-  var result, translation;
-  translations = "";
+	content = original;
+	// console.log(content);
+	var regex = new RegExp(/^([A-Za-z\-/ ]+): (.+)$/gm);
+	var result, translation;
+	translations = "";
 
-  while ((result = regex.exec(content)) !== null) {
-    let name = result[1].trim();
-    let value = result[2].trim();
-    // console.log(name, " : ", value);
+	while ((result = regex.exec(content)) !== null) {
+		let name = result[1].trim();
+		let value = result[2].trim();
+		// console.log(name, " : ", value);
 
-    translation = "";
-    const row = db.prepare('SELECT * FROM glossary WHERE en = ? COLLATE NOCASE').get(name);
-    if (row !== undefined) {
-      if (row.type == "property") {
-        translation = row.ja;
-      }
-    } else {
-      translation = name;
-    }
+		translation = "";
+		const row = db.prepare('SELECT * FROM glossary WHERE en = ? COLLATE NOCASE').get(name);
+		if (row !== undefined) {
+			if (row.type == "property") {
+				translation = row.ja;
+			}
+		} else {
+			translation = name;
+		}
 
-    translation = translation + "：";
+		translation = translation + "：";
 
-    const row2 = db.prepare('SELECT * FROM glossary WHERE en = ? COLLATE NOCASE').get(value);
-    if (row2 !== undefined) {
-      translation = translation + row2.ja + "\n";
-    } else {
-      translation = translation + value + "\n"
-    }
+		const row2 = db.prepare('SELECT * FROM glossary WHERE en = ? COLLATE NOCASE').get(value);
+		if (row2 !== undefined) {
+			translation = translation + row2.ja + "\n";
+		} else {
+			translation = translation + value + "\n"
+		}
 
-    translations = translations + translation
+		translations = translations + translation
 
-  }
-  return translations
+	}
+	return translations
 }
 
 
 function getLocalIp() {
-  const os = require('os');
+	const os = require('os');
 
-  for (let addresses of Object.values(os.networkInterfaces())) {
-    for (let add of addresses) {
-      if (add.address.startsWith('192.168.')) {
-        return add.address;
-      }
-    }
-  }
+	for (let addresses of Object.values(os.networkInterfaces())) {
+		for (let add of addresses) {
+			if (add.address.startsWith('192.168.')) {
+				return add.address;
+			}
+		}
+	}
 }
--- public/js/script.js
+++ public/js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 			})
 			.done(function (data) {
 				console.log("returned data:", data);
-				$('#translation').html(data.message);
+				$('#translation').html(data.translation);
 			});
 	});
 
@@ -54,3 +54,89 @@ $(document).ready(function () {
 	});
 
 });
+
+
+
+
+$(document).ready(function(){
+    $(document).on('show.bs.modal','#myModal', function () {
+		// Use try & catch for unsupported browser
+		var text = document.getElementById("original");
+		var t = text.value.substr(text.selectionStart, text.selectionEnd - text.selectionStart);
+		if (t.trim().length == 0) {
+			text = document.getElementById("translation");
+			t = text.value.substr(text.selectionStart, text.selectionEnd - text.selectionStart);
+		}
+
+		if (t.trim().length != 0) {
+			console.log(t)
+			document.getElementById("en").value = t.trim();
+		}
+
+		/* try {
+			// The important part (copy selected text)
+			var ok = document.execCommand('copy');
+
+			if (ok) console.log('Copied!');
+			else    console.log('Unable to copy!');
+		} catch (err) {
+			console.log('Unsupported Browser!');
+		} */
+
+		 $('#myModal').show();
+    });
+
+	$('#myModal').on('hidden.bs.modal', function () {
+		$("#addTerm").trigger("reset"); // Reset form
+	});
+
+});
+ 
+// [{"en":"silk","ja":"正絹","furigana":"","romaji":"shouken","ja2":"","en2":"","context":"","type":"material","note":""},{"en":"silk","ja":"絹","furigana":"きぬ","romaji":"kinu","ja2":"","en2":"","context":"","type":"material","note":""}]
+
+function updateTable(term) {
+	console.log(term);
+	document.getElementById("wrapper").innerHTML = '';
+	$.ajax({
+		url: "/api/getrows",
+		data: {
+			term: term
+		},
+		success: function () {
+			console.log('Successfully connected to the server');
+		},
+		error: function () {
+			console.log('Something went wrong');
+		}
+	})
+	.done(function (data) {
+		rows = data.rows; 
+		console.log(rows);
+
+		const grid = new gridjs.Grid({
+			sort: true,
+			resizable: true,
+			search: true,
+			pagination: {
+				limit: 50
+				},
+			fixedHeader: true,
+			height: '400px',  
+			data:  rows	  }).render(document.getElementById("wrapper"));
+
+		/* grid.updateConfig({
+			sort: true,
+			resizable: true,
+			search: true,
+			pagination: {
+				limit: 50
+				},
+			fixedHeader: true,
+			height: '400px',		
+			data: rows
+		}).forceRender(); */
+	});
+
+
+
+}
